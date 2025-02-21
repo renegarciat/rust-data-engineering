2027d22ce8a1568f048ba63ca8e883cb3abb6c8b
Garcia,Rene <rene.orlando.garcia@aptiv.com>
2025-02-21 15:18:39 -0600

    Lab: Command Line Fruit Salad Creator

--- cli-salad/src/main.rs
+++ cli-salad/src/main.rs
@@ -1,6 +1,6 @@
 use clap::Parser;
 use cli_salad::create_fruit_salad;
-
+/* Try running ./cli-salad -n 3 -f 'myfruit1 myfruit2 myfruit3' */
 #[derive(Parser)]
 #[clap(
     version = "1.0",
@@ -10,6 +10,15 @@ use cli_salad::create_fruit_salad;
 struct Opts {
     #[clap(short, long)]
     number: usize,
+    #[clap(short, long, value_delimiter = ' ', num_args = 1..)]
+    pub fruits: Option<Vec<String>>,
+}
+
+fn print_fruits(fruits: &Vec<String>) {
+    // locally sort the fruits
+    let mut local_fruits = fruits.clone();
+    local_fruits.sort_by_key(|word| word.to_lowercase());
+    println!("Sorted fruits: {:?}", local_fruits);
 }
 
 fn main() {
@@ -18,13 +27,19 @@ fn main() {
     // Get the number of fruits the user requested
     let num_fruits = opts.number;
 
+    // Get the inputed fruits
+    let mut in_fruits = opts.fruits.unwrap();
+    println!("Input fruits: {:?}", in_fruits);
     // Create the fruit salad
-    create_fruit_salad(num_fruits);
+    let mut my_salad = create_fruit_salad(num_fruits);
+    my_salad.append(&mut in_fruits);
 
     // Print the fruit salad in human readable format with a count of fruits used
     println!(
         "Created Fruit salad with {} fruits: {:?}",
-        num_fruits,
-        create_fruit_salad(num_fruits)
+        num_fruits, my_salad
     );
+
+    // Sort and print the fruits
+    print_fruits(&my_salad);
 }
